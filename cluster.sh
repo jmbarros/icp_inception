c1e3c2026944bb02c21ef89d23189a0f5f169357
JMBarros <jmbarros@users.noreply.github.com>
2017-09-20 00:38:17 -0300

    add hosts creation

---
+++ cluster.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+cd ~ 
+/usr/bin/docker run -e LICENSE=accept -v "$(pwd)":/data ibmcom/cfc-installer:2.1.0-beta-1 cp -r cluster /data
+cat ~/.ssh/id_rsa > ~/cluster/ssh_key
+echo "" > ~/cluster/hosts
+ansible ~/icp_inception/hosts.yml -i ~/icp_inception/inventory
+cd ~/cluster
+/usr/bin/docker run -e LICENSE=accept --net=host -t -v "$(pwd)":/installer/cluster ibmcom/cfc-installer:2.1.0-beta-1 install
