7c93b76ad67e29ca90e100d3b3e0668af2a0b4e4
JMBarros <jmbarros@users.noreply.github.com>
2017-09-20 00:54:06 -0300

    sintax

--- cluster.sh
+++ cluster.sh
@@ -3,6 +3,6 @@ cd ~
 /usr/bin/docker run -e LICENSE=accept -v "$(pwd)":/data ibmcom/cfc-installer:2.1.0-beta-1 cp -r cluster /data
 cat ~/.ssh/id_rsa > ~/cluster/ssh_key
 echo "" > ~/cluster/hosts
-ansible ~/icp_inception/hosts.yml -i ~/icp_inception/inventory
+/usr/bin/ansible-playbook ~/icp_inception/hosts.yml -i ~/icp_inception/inventory
 cd ~/cluster
 /usr/bin/docker run -e LICENSE=accept --net=host -t -v "$(pwd)":/installer/cluster ibmcom/cfc-installer:2.1.0-beta-1 install
